d85366174ff17f62a832b978a26f9d348a96ca4d
imonous <mykolassle@gmail.com>
2023-09-30 19:35:31 +0200

    Removed unnecessary files.

--- lab/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import index, simulation
-
-urlpatterns = [
-    path("", index, name="index"),
-    path("simulation", simulation, name="simulation"),
-]
