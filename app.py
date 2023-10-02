8d2253519624a2e8d8dc6e1ef6f9fd9ccd51c951
imonous <mykolassle@gmail.com>
2023-10-02 21:02:11 +0200

    Added basic simulation.

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
 import panel as pn
 import holoviews as hv
+from holoviews.streams import Pipe
 from streamz import Stream
+
 from simulator import Simulation
 
+pn.extension("plotly")
 hv.extension("plotly")
 
-sim = Simulation(500, 1.2e-20, 0.01, 500, 2, 0.05)
+TIME_STEP = 0.2
+PARTICLE_COUNT = 1000
+GAS_VOLUME = 2
+GAS_SIDE_LEN = 2 ** (1 / 3) / 2
+
+sim = Simulation(PARTICLE_COUNT, 1.2e-20, 0.01, 500, GAS_VOLUME, TIME_STEP)
+source = Stream()
 
 
-# def display_gas(data):
-#     hv.Scatter3D((data[:, 0], data[:, 1], data[:, 2])).opts(
-#         size=5, height=600, width=600
-#     )
+def display_gas(data):
+    axis_lim = (-GAS_SIDE_LEN, GAS_SIDE_LEN)
+    return hv.Scatter3D(data).opts(size=3, xlim=axis_lim, ylim=axis_lim, zlim=axis_lim)
 
 
-# source = Stream()
-# stream = source.map(display_gas)
+def emit():
+    sim.next_step()
+    source.emit((sim.r[:, 0], sim.r[:, 1], sim.r[:, 2]))
 
 
-# def emit():
-#     sim.next_step()
-#     source.emit(sim.r)
+gas_display_stream = source.map(display_gas)
+gas_display_pane = pn.pane.Streamz(gas_display_stream, height=350)
 
-
-# gas_display = pn.pane.Streamz(stream, height=350)
-# pn.state.add_periodic_callback(emit, period=1000, count=5)
-
-# gas_display.servable()
+layout = pn.Row(gas_display_pane)
+pn.state.add_periodic_callback(emit, period=int(TIME_STEP * 10**3))
+layout.servable()
