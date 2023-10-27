3a136092c26fa4d37d1663bfdc0b44752b9ebd46
imonous <mykolassle@gmail.com>
2023-10-27 18:53:39 +0200

    Added multiprocessing to detached simulator.

--- detached_simulator.py
+++ detached_simulator.py
@@ -17,27 +17,34 @@
 
 """
 
-
-from simulation import Simulation
 import numpy as np
+import multiprocessing
+from functools import partial
+from simulation import Simulation
 
 
-def run(sim, total_iter, out_file_path, log_file=None, log_iter=0):
+def run(sim, out_file_path, total_iter, log_file_path=None, log_iter=0):
     """
     sim: the simulation object
-    total_iter: total number of iterations
     out_file_path: the output file path (append mode) (more convienient to accept string)
+    total_iter: total number of iterations
     [log_iter]: how often to log (percentage as decimal)
     [log_file]: log file (more convienient to accept file object)
     """
+    if log_iter:
+        log_file = open(log_file_path, "a")
     n = int(log_iter * total_iter)
     for i in range(total_iter):
         sim.next_step()
-        if n != 0 and i % n == 0:
+        if log_iter and i % n == 0:
             progress = round(log_iter * (i // n), 2)
             log_file.write(f"{out_file_path}: {progress * 100}%\n")
-        elif n != 0 and i == total_iter - 1:
+            log_file.flush()
+        elif log_iter and i == total_iter - 1:
             log_file.write(f"{out_file_path}: DONE!\n")
+            log_file.flush()
+    if log_iter:
+        log_file.close()
     sim.save_object(out_file_path)
 
 
@@ -47,7 +54,7 @@ if __name__ == "__main__":
     RANGE = (200, 2000)
     TRIALS = 2  # 5
     TEMP_COUNT = 2  # 10
-    ITER_COUNT = 10  # 10**4
+    ITER_COUNT = 10**3  # 10**4
 
     # Simulation constants
     PARTICLE_COUNT = 1000
@@ -56,12 +63,9 @@ if __name__ == "__main__":
     GAS_VOLUME = 1
     TIME_STEP = 0.01
 
-    with open("./data/log.txt", "w"):  # clear log contents
-        pass
-    log_file = open("./data/log.txt", "a")
+    args = []
     for temp in np.linspace(*RANGE, TEMP_COUNT):
         for trial in range(1, TRIALS + 1):
-            file_path = f"./data/temp{int(temp)}trial{trial}.pkl"
             sim = Simulation(
                 n_particles=PARTICLE_COUNT,
                 mass=PARTICLE_MASS,
@@ -70,5 +74,16 @@ if __name__ == "__main__":
                 dt=TIME_STEP,
                 V=GAS_VOLUME,
             )
-            run(sim, ITER_COUNT, file_path, log_file, log_iter=0.2)
-    log_file.close()
+            file_path = f"./data/temp{int(temp)}trial{trial}.pkl"
+            args.append((sim, file_path))
+
+    log_file_path = "./data/log.txt"
+    with open(log_file_path, "w"):  # clear log contents
+        pass
+    func = partial(
+        run, total_iter=ITER_COUNT, log_file_path=log_file_path, log_iter=0.2
+    )
+    with multiprocessing.Pool(processes=2) as pool:
+        pool.starmap(func, args)
+        pool.close()
+        pool.join()
