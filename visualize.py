c5c85b60920b4ff53648b4fc7432fc05c6c8abb9
imonous <mykolassle@gmail.com>
2023-11-18 15:09:53 +0100

    Added uncertainties to the table.

--- visualize.py
+++ visualize.py
@@ -7,13 +7,16 @@ def hide_table_index(plot, element):
     plot.handles["table"].index_position = None
 
 
-def table(data):
-    heads = ["T"] + [f"Trial {i+1}" for i in range(len(data[0]) - 1)]
-    return hv.Table(data, heads).opts(hooks=[hide_table_index])
+def table(data, uncertainty=False):
+    offset = 3 if uncertainty else 1  # account for non-trials
+    heads = ["Temperature"] + [f"Trial {i+1}" for i in range(len(data[0]) - offset)]
+    if uncertainty:
+        heads += ["Absolute uncert.", "Relative uncert."]
+    return hv.Table(data, heads).opts(hooks=[hide_table_index], width=800)
 
 
 def scatter(data):
-    return hv.Scatter(data)
+    return hv.Scatter(data, kdims=["T"], vdims="Part")
 
 
 def curve(data):
